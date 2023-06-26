c2477ae135228a9e602e6df85be261e54c6ba181
Sason <sasonreza@gmail.com>
2023-06-25 21:23:31 -0700

    Add API endpoints for retrieving individual insights

--- app/routes/insights.py
+++ app/routes/insights.py
@@ -1,14 +1,59 @@
+from functools import lru_cache
 from flask import Blueprint, jsonify, request
-from app.services.insights_service import get_top_tracks
 
-insights_bp = Blueprint("insights", __name__)
+from app.services.insights_service import (
+    get_top_artists,
+    get_top_primary_artists,
+    get_top_tracks,
+)
+
+insights_bp = Blueprint("insights", __name__, url_prefix="/insights")
 
 
+# Function to get top tracks, given a user ID
+# Example: http://songwap.social/insights/top-tracks?user_id=1
 @insights_bp.route("/top-tracks", methods=["GET"])
+@lru_cache(maxsize=10)
 def top_tracks():
     user_id = request.args.get("user_id")
     if not user_id:
         return jsonify({"error": "Missing user_id parameter"}), 400
 
-    tracks = get_top_tracks(user_id)
+    limit = request.args.get("limit")
+    if limit:
+        # Try converting to int
+        try:
+            limit = int(limit)
+            if limit < 1 or limit > 100:
+                return jsonify({"error": "Limit must be between 1 and 100"}), 400
+        except ValueError:
+            return jsonify({"error": "Invalid limit parameter"}), 400
+
+    tracks = get_top_tracks(user_id, limit)
     return jsonify(tracks), 200
+
+
+# Function to get top artists, given a user ID
+# Example: http://songwap.social/insights/top-artists?user_id=1
+@insights_bp.route("/top-artists", methods=["GET"])
+@lru_cache(maxsize=10)
+def top_artists():
+    user_id = request.args.get("user_id")
+    if not user_id:
+        return jsonify({"error": "Missing user_id parameter"}), 400
+
+    artists = get_top_artists(user_id)
+    return jsonify(artists), 200
+
+
+# Function to get top primary artists, given a user ID
+# Example: http://songwap.social/insights/top-primary-artists?user_id=1
+@insights_bp.route("/top-primary-artists", methods=["GET"])
+@lru_cache(maxsize=10)
+def top_primary_artists():
+    user_id = request.args.get("user_id")
+    if not user_id:
+        return jsonify({"error": "Missing user_id parameter"}), 400
+
+    artists = get_top_primary_artists(user_id)
+    return jsonify(artists), 200
--- app/services/insights_service.py
+++ app/services/insights_service.py
@@ -1,16 +1,79 @@
+from typing import List
+
 from app import db
-from app.models.history import History
+from app.models.history import Artists, ArtistTracks, History, Tracks
 
 
-def get_top_tracks(user_id):
-    result = (
-        db.session.query(History.track_id, db.func.count(History.track_id))
+def get_total_tracks(user_id: int) -> int:
+    return db.session.query(History).filter(History.user_id == user_id).count()
+
+
+def def_distinct_tracks(user_id: int) -> int:
+    return (
+        db.session.query(History)
         .filter(History.user_id == user_id)
         .group_by(History.track_id)
+        .count()
+    )
+
+
+def get_top_tracks(user_id: int, limit: int = 10) -> List[dict]:
+    result = (
+        db.session.query(
+            History.track_id, Tracks.name, Artists.name, db.func.count(History.track_id)
+        )
+        .join(Tracks, Tracks.id == History.track_id)
+        .join(ArtistTracks, ArtistTracks.track_id == Tracks.id)
+        .join(Artists, Artists.id == ArtistTracks.artist_id)
+        .filter(History.user_id == user_id)
+        .filter(ArtistTracks.is_primary == True)
+        .group_by(History.track_id, Tracks.name, Artists.name)
+        .order_by(db.func.count(History.track_id).desc())
+        .limit(limit)
+        .all()
+    )
+
+    top_tracks = [
+        {"track_id": r[0], "track_name": r[1], "artist_name": r[2], "count": r[3]}
+        for r in result
+    ]
+    return top_tracks
+
+
+def get_top_artists(user_id: int) -> List[dict]:
+    result = (
+        db.session.query(Artists.id, Artists.name, db.func.count(History.track_id))
+        .join(ArtistTracks, ArtistTracks.artist_id == Artists.id)
+        .join(Tracks, Tracks.id == ArtistTracks.track_id)
+        .join(History, History.track_id == Tracks.id)
+        .filter(History.user_id == user_id)
+        .group_by(Artists.id, Artists.name)
         .order_by(db.func.count(History.track_id).desc())
         .limit(10)
         .all()
     )
 
-    top_tracks = [{"track_id": r[0], "count": r[1]} for r in result]
-    return top_tracks
+    top_artists = [
+        {"artist_id": r[0], "artist_name": r[1], "count": r[2]} for r in result
+    ]
+    return top_artists
+
+
+def get_top_primary_artists(user_id: int) -> List[dict]:
+    result = (
+        db.session.query(Artists.id, Artists.name, db.func.count(History.track_id))
+        .join(ArtistTracks, ArtistTracks.artist_id == Artists.id)
+        .join(Tracks, Tracks.id == ArtistTracks.track_id)
+        .join(History, History.track_id == Tracks.id)
+        .filter(History.user_id == user_id)
+        .filter(ArtistTracks.is_primary == True)
+        .group_by(Artists.id, Artists.name)
+        .order_by(db.func.count(History.track_id).desc())
+        .limit(10)
+        .all()
+    )
+
+    top_artists = [
+        {"artist_id": r[0], "artist_name": r[1], "count": r[2]} for r in result
+    ]
+    return top_artists
